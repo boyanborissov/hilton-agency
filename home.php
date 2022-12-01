9bdb88bf3e2a8942feb8a9a754401b347f72a111
Dimitar Stefanov <dimistef6969@gmail.com>
2022-12-01 13:24:09 +0200

    Updated

--- home.php
+++ home.php
@@ -24,7 +24,7 @@
 
         <link rel="stylesheet" href="css/home.css?<?php echo time();?>">
     </head>
-<body>
+<body onload="video()">
     <!--Navbar-->
     <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
         <div class="container" id="myDiv">
@@ -84,11 +84,13 @@
 
 <!-- Fixed Video Height-->
 <script>
-    let height = window.innerHeight;
-    var clientHeight = document.getElementById('myDiv').clientHeight;
+    function video() {
+        let height = window.innerHeight;
+        var clientHeight = document.getElementById('myDiv').clientHeight;
 
-    var vh = height - clientHeight + 70;
-    document.getElementById("video").style.height = "" + vh + "px";
+        var vh = height - clientHeight + 70;
+        document.getElementById("video").style.height = "" + vh + "px";
+    }
 </script>
 
 <!--About section -->
@@ -210,8 +212,6 @@
         <p class="text-white">All Right Reserved &copy;THE HILTON AGENCY</p>
     </div>
 </footer>
-
-
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" 

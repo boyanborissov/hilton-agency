bdd1247da9156485a9319f530c45faa923ead45b
Boyan <boyborissov@gmail.com>
2022-12-20 23:51:48 +0200

    Updated

--- aboutus.php
+++ aboutus.php
@@ -1,82 +1,69 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <title>The Hilton Agency</title>
-    
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" 
-        rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" 
-        crossorigin="anonymous">
-    
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" 
-        crossorigin="anonymous"></script>
-    
-        <script src="https://kit.fontawesome.com/8831516dec.js" crossorigin="anonymous"></script>
-    
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="css/aboutus.css?<?php echo time();?>">
-    
-    </head>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>The Hilton Agency</title>    
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">  
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/aboutus.css?<?php echo time();?>">
+</head>
 <body>
     <!--Navbar-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
-        <div class="container">
-          <a class="navbar-brand" href="home.php"><b>THE HILTON AGENCY</b></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="home.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="aboutus.php">About us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Properties</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="agents.php">Our Agents</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="blogpage.php">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact_form.php">Contact us</a>
-              </li>
-              <?php
-                if(isset($_SESSION['username'])) {
-                    echo '<li class="nav-item">
-                            <a class="nav-link" href="#">'. $_SESSION['username'] .'</a>
-                          </li>
-                          <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Logout</a>
-                          </li>';
-                } else {
-                    echo '<li class="nav-item">
-                            <a class="nav-link" href="login.php">Log In</a>
-                          </li>
-                          <li class="nav-item">
-                            <a class="nav-link" href="register.php">Register</a>
-                          </li>';
-                }
-              ?>
-              
-            </ul>
-          </div>
-        </div>
-      </nav>
-    <!--About us-->
-    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+  <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="home.php"><b>THE HILTON AGENCY</b></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="home.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="aboutus.php">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Properties</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="agents.php">Our Agents</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blogpage.php">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact_form.php">Contact us</a>
+            </li>
+            <?php
+              if(isset($_SESSION['username'])) {
+                  echo '<li class="nav-item">
+                          <a class="nav-link" href="#">'. $_SESSION['username'] .'</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="logout.php">Logout</a>
+                        </li>';
+              } else {
+                  echo '<li class="nav-item">
+                          <a class="nav-link" href="login.php">Log In</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="register.php">Register</a>
+                        </li>';
+              }
+          ?>  
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!--About us-->
+  <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -111,52 +98,51 @@
       <span class="visually-hidden">Next</span>
     </button>
   </div>
-    <!-- <section id="about">
-        <div class="about-1">
-            <h1>ABOUT US</h1>
-        </div>
-        <div id="about-2">
-            <div class="content-box-lg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="about-item text-center">
-                                <i class="fa fa-book"></i>
-                                <h3>MISSION</h3>
-                                <hr>
-                                <p>“Delivering measurable results to clients, employees, and vendors.” “Provide the best customer results possible and deliver the WOW factor through our services.” “Our mission is to be the experts in marketing and sales alignment and the masters of the message</p>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="about-item text-center">
-                                <i class="fa fa-book"></i>
-                                <h3>VISSION</h3>
-                                <hr>
-                                <p>Put simply, your mission statement focuses on today and what you are doing to achieve the promises made. Your agency’s vision focuses on tomorrow and what the goal of the company is.While these are two very different concepts, the line often gets blurred. </p>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="about-item text-center">
-                                <i class="fa fa-book"></i>
-                                <h3>ACHIEVMENT</h3>
-                                <hr>
-                                <p>When an agency lands a prestigious award, a grant or a big investor, it can be a unique marketing opportunity for the company. Regardless of what the honor or accomplishment is, having the business recognized lends credibility. An agency drive both sales leads and loyalty.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
-    <!--footer-->
-<footer class="bg-dark p-2 text-center">
+  <!-- <section id="about">
+      <div class="about-1">
+          <h1>ABOUT US</h1>
+      </div>
+      <div id="about-2">
+          <div class="content-box-lg">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-md-4">
+                          <div class="about-item text-center">
+                              <i class="fa fa-book"></i>
+                              <h3>MISSION</h3>
+                              <hr>
+                              <p>“Delivering measurable results to clients, employees, and vendors.” “Provide the best customer results possible and deliver the WOW factor through our services.” “Our mission is to be the experts in marketing and sales alignment and the masters of the message</p>
+                          </div>
+                      </div>
+                      <div class="col-md-4">
+                          <div class="about-item text-center">
+                              <i class="fa fa-book"></i>
+                              <h3>VISSION</h3>
+                              <hr>
+                              <p>Put simply, your mission statement focuses on today and what you are doing to achieve the promises made. Your agency’s vision focuses on tomorrow and what the goal of the company is.While these are two very different concepts, the line often gets blurred. </p>
+                          </div>
+                      </div>
+                      <div class="col-md-4">
+                          <div class="about-item text-center">
+                              <i class="fa fa-book"></i>
+                              <h3>ACHIEVMENT</h3>
+                              <hr>
+                              <p>When an agency lands a prestigious award, a grant or a big investor, it can be a unique marketing opportunity for the company. Regardless of what the honor or accomplishment is, having the business recognized lends credibility. An agency drive both sales leads and loyalty.</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section> -->
+  <!--footer-->
+  <footer class="bg-dark p-2 text-center">
   <div class="container">
-      <p class="text-white">All Right Reserved &copy;THE HILTON AGENCY</p>
+    <p class="text-white">All Right Reserved &copy;THE HILTON AGENCY</p>
   </div>
-</footer>
+  </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
-		  integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" 
-		  crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/8831516dec.js" crossorigin="anonymous"></script>
 </body>
 </html>
--- login.php
+++ login.php
@@ -2,46 +2,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/main.css?<?php echo time();?>">
-  <link rel="stylesheet" href="css/login.css?<?php echo time();?>">
-  <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css?<?php echo time();?>">
+    <link rel="stylesheet" href="css/login.css?<?php echo time();?>">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
 </head>
 <body class="text-center">
-    <main class="form-signin w-100 m-auto">
-        <form method="get" action="logprocess.php" autocomplete="off">
-          <h1 class="h2 mb-4 fw-normal">The Hilton Agency</h1>
-
-          <h1 class="h3 mb-4 fw-normal">Log In</h1>
-      
-          <!--Input Fields-->
-          <div class="form-floating">
-            <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating mt-3">
-            <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
-          
-          <p class="mt-3 mb-2 text-primary">Not a member?<a href="register.php" class="mt-5 mb-2">   Register Now!</a></p>
-      
-          <button class="btn" type="submit">Sign in</button>
-          <p class="mt-4 mb-3 text-primary">All Right Reserved &copy;THE HILTON AGENCY</p>
-
-          <div class="form-floating mt-3">
-            <?php
-                if (isset($_SESSION["loginError"])) {
-                  echo "<p style='color:red'> Invalid input! </p>";
-                }   
-                unset($_SESSION["loginError"]);      
-            ?>
-          </div>
-        </form>
-    </main>
+  <main class="form-signin w-100 m-auto">
+    <form method="get" action="logprocess.php" autocomplete="off">
+      <h1 class="h2 mb-4 fw-normal">The Hilton Agency</h1>
+      <h1 class="h3 mb-4 fw-normal">Log In</h1>
+      <!--Input Fields-->
+      <div class="form-floating">
+        <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating mt-3">
+        <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>    
+      <p class="mt-3 mb-2 text-primary">Not a member?<a href="register.php" class="mt-5 mb-2">   Register Now!</a></p>
+      <button class="btn" type="submit">Sign in</button>
+      <p class="mt-4 mb-3 text-primary">All Right Reserved &copy;THE HILTON AGENCY</p>
+      <div class="form-floating mt-3">
+        <?php
+          if (isset($_SESSION["loginError"])) {
+            echo "<p style='color:red'> Invalid input! </p>";
+          }   
+          unset($_SESSION["loginError"]);      
+        ?>
+      </div>
+    </form>
+  </main>
 </body>
 </html>

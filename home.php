11547a36a477151c16247e3e2d9b3524331c54c1
Boyan <boyborissov@gmail.com>
2023-02-09 18:11:01 +0200

    Updated

--- home.php
+++ home.php
@@ -11,7 +11,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/home.css?<?php echo time();?>">
+  <link rel="stylesheet" href="css/home.css?php echo time();?>">
 </head>
 <body onload="video()">
     <!--Navbar-->

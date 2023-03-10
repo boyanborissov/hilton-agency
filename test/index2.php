f2783554a3c19406a1b42a7e49b8ff0632924c8f
Boyan <boyborissov@gmail.com>
2023-03-10 12:09:42 +0200

    Updated

---
+++ test/index2.php
@@ -0,0 +1,54 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>The Hilton Agency</title>
+  <link rel="icon" href="images/logo.png">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style2.css?<?php echo time();?>">
+</head>
+<body>
+  <!--Navbar-->  
+    <!--Properties-->
+  <div class="container">
+    <div class="title">
+      <h1>Our Properties</h1>
+    <hr>
+    </div>
+    <h2>Opacity with Box</h2>
+<p>Hover over the image to see the effect.</p>
+
+<div class="container">
+  <img src="avatar.png" alt="Avatar" class="image" style="width:100%">
+  <div class="middle">
+    <div class="text">John Doe</div>
+  </div>
+</div>
+  <!--footer-->
+  <footer class="bg-dark p-2 text-center">
+    <div class="container">
+      <p class="text-white">All Right Reserved &copy;THE HILTON AGENCY</p>
+    </div>
+  </footer>
+  <script>
+        const nav2 = document.querySelector(".navbar");
+
+        window.addEventListener("scroll", () => {
+            if(window.scrollY >= 50){
+                nav2.classList.add('bg-dark');
+            }else if(window.scrollY < 50){
+                nav2.classList.remove('bg-dark');
+            }
+        })
+    </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/8831516dec.js" crossorigin="anonymous"></script>
+</body>
+</html>
